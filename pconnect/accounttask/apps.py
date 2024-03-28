6cf1e70a358c636090ef21558e7890eb3c34a23d
MSukpirom <montirasukpirom@gmail.com>
2024-03-29 00:41:26 +0700

    28

--- pconnect/accounttask/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AccounttaskConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'accounttask'
---
+++ pconnectacc/taskcontrol/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from .views import *
+
+App_name = "taskcontrol"
+
+urlpatterns = [
+    # path('', home),
+]
