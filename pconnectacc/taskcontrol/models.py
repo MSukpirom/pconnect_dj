bc634e00e33f85ca02bb45afd28a96c37e35df1f
MSukpirom <montirasukpirom@gmail.com>
2024-03-29 13:28:53 +0700

    29

--- pconnectacc/taskcontrol/models.py
+++ pconnectacc/taskcontrol/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-
-# Create your models here.
+from django.conf import settings
+from django.utils import timezone
 
 class Country(models.Model):
     name_th =  models.CharField(max_length=255, default=None, null=True, blank=True)
@@ -15,3 +15,310 @@ class Geography(models.Model):
 
     class Meta:
         db_table = 'geography'
+
+class AddressBase(models.Model):
+    address = models.CharField(max_length=255, default=None, null=True, blank=True)
+    province = models.ForeignKey('Province', on_delete=models.CASCADE, blank=True, null=True)
+    district = models.ForeignKey('District', on_delete=models.CASCADE, blank=True, null=True)
+    subdistrict = models.ForeignKey('Subdistrict', on_delete=models.CASCADE, blank=True, null=True)
+    client = models.ForeignKey('Client', on_delete=models.CASCADE, blank=True, null=True, related_name='client_addressbase')
+    contact = models.ForeignKey('Contact', on_delete=models.CASCADE, blank=True, null=True, related_name='contact_addressbase')
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'address_base'
+
+class Client(models.Model):
+    code = models.CharField(max_length=255, unique=True, default=None, null=True, blank=True)
+    company_name = models.CharField(max_length=255, default=None, null=True, blank=True)
+    tax_id = models.CharField(max_length=255, default=None, null=True, blank=True)
+    service_fee = models.DecimalField(max_digits=16, decimal_places=2, blank=True, null=True)
+    create_client_date = models.DateField(blank=True, null=True)
+    channal = models.CharField(max_length=255, default=None, null=True, blank=True)
+    detail = models.CharField(max_length=255, default=None, null=True, blank=True)
+    contact = models.ForeignKey('Contact', on_delete=models.CASCADE, blank=True, null=True, related_name='client_contact')
+    status = models.BooleanField(default=False)
+    register_tax = models.ForeignKey('RegisterTax', on_delete=models.CASCADE, blank=True, null=True, related_name='client_register_tax')
+    company_address = models.ForeignKey(AddressBase, on_delete=models.CASCADE, blank=True, null=True, related_name='client_company_address')
+    file = models.ForeignKey('FileManage', on_delete=models.CASCADE, blank=True, null=True, related_name='client_file')
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'client'
+
+class FileManage(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, blank=True, null=True)
+    file_client = models.FileField(upload_to='files/client/', null=True, max_length=255, blank=True)
+    image_client = models.ImageField(upload_to='images/client/', null=True, blank=True)
+    engagement = models.ForeignKey('Engagement', on_delete=models.CASCADE, blank=True, null=True)
+    file_engagement = models.FileField(upload_to='files/engagement/', null=True, max_length=255, blank=True)
+    image_engagement = models.ImageField(upload_to='images/engagement/', null=True, blank=True)
+    task = models.ForeignKey('Task', on_delete=models.CASCADE, blank=True, null=True)
+    file_task = models.FileField(upload_to='files/task/', null=True, max_length=255, blank=True)
+    image_task = models.ImageField(upload_to='images/task/', null=True, blank=True)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'file_manage'
+
+class Province(models.Model):
+    name_th =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_en =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    geography = models.ForeignKey(Geography, on_delete=models.CASCADE, blank=True, null=True, related_name='provinces')
+    country = models.ForeignKey(Country, on_delete=models.CASCADE, blank=True, null=True, related_name='provinces')
+
+    class Meta:
+        db_table = 'province'
+
+class District(models.Model):
+    name_th =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_en =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, blank=True, null=True, related_name='districts')
+
+    class Meta:
+        db_table = 'district'
+
+class Subdistrict(models.Model):
+    name_th =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_en =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    zipcode = models.PositiveIntegerField()
+    district = models.ForeignKey(District, on_delete=models.CASCADE, blank=True, null=True)
+
+    class Meta:
+        db_table = 'subdistrict'
+
+class RegisterTax(models.Model):
+    vat = models.CharField(max_length=255, default=None, null=True, blank=True)
+    vat_date = models.DateField(blank=True, null=True)
+    sbt = models.CharField(max_length=255, default=None, null=True, blank=True)
+    sbt_date = models.DateField(blank=True, null=True)
+    sso = models.CharField(max_length=255, default=None, null=True, blank=True)
+    sso_date = models.DateField(blank=True, null=True)
+    dbd_e_filling = models.CharField(max_length=255, default=None, null=True, blank=True)
+    dbd_e_filling_date = models.DateField(blank=True, null=True)
+    company = models.CharField(max_length=255, default=None, null=True, blank=True)
+    company_date = models.DateField(blank=True, null=True)
+    company_period_date = models.CharField(max_length=255, default=None, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'register_tax'
+
+class RegisterType(models.Model):
+    short_name = models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_th =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_en =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'register_type'
+
+class Contact(models.Model):
+    client = models.ForeignKey(Client, on_delete=models.CASCADE, related_name='contact_client')
+    name = models.CharField(max_length=255, default=None, null=True, blank=True)
+    position = models.CharField(max_length=255, default=None, null=True, blank=True)
+    phone = models.CharField(max_length=255, default=None, null=True, blank=True)
+    email = models.EmailField(default=None, null=True, blank=True)
+    line = models.CharField(max_length=255, default=None, null=True, blank=True)
+    other = models.CharField(max_length=255, default=None, null=True, blank=True)
+    address = models.ForeignKey(AddressBase, on_delete=models.CASCADE, blank=True, null=True, related_name='contact_address')
+    address2 = models.ForeignKey(AddressBase, on_delete=models.CASCADE, blank=True, null=True, related_name='company_address')
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'contact'
+
+class ClientPassword(models.Model):
+    client = models.ForeignKey('Client', null=True, blank=True, on_delete=models.CASCADE)
+    type_password = models.ForeignKey('RegisterType', null=True, blank=True, on_delete=models.CASCADE)
+    username =  models.CharField(max_length=255, default=None, null=True, blank=True)
+    password = models.TextField(default=None, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    
+    class Meta:
+        db_table = 'client_password'
+
+class Engagement(models.Model):
+    client = models.ForeignKey('Client', on_delete=models.CASCADE)
+    job_code = models.CharField(max_length=255, unique=True)
+    start_date_service = models.DateField()
+    end_date_service = models.DateField()
+    start_date_period = models.DateField()
+    end_date_period = models.DateField()
+    administrator = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='admin_engagements')
+    approver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='approver_engagements',null=True, blank=True)
+    approved_date = models.DateTimeField(null=True, blank=True)
+    reviewer = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='reviewer_engagements',null=True, blank=True)
+    review_date = models.DateTimeField(auto_now_add=True)
+    status = models.CharField(max_length=255, default=None, null=True, blank=True)
+    file = models.ForeignKey('FileManage', on_delete=models.CASCADE, related_name='engagement_file', null=True, blank=True)
+    create_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='engagement_create_by')
+    create_at = models.DateTimeField(auto_now_add=True)
+    update_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, blank=True, null=True, related_name='engagement_update_by')
+    update_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey('EngagementCategory', on_delete=models.CASCADE, related_name='engagement_category', null=True, blank=True)
+    type = models.ForeignKey('EngagementType', on_delete=models.CASCADE, related_name='engagement_type', null=True, blank=True)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'engagement'
+
+    def update_status(self):
+        # นับจำนวน Task ที่เป็นแต่ละสถานะ
+        open_jobs = self.task_set.filter(status='OPEN JOB').count()
+        in_progress = self.task_set.filter(status='IN PROGRESS').count()
+        review = self.task_set.filter(status='REVIEW').count()
+        pending_client = self.task_set.filter(status='PENDING CLIENT').count()
+        done = self.task_set.filter(status='DONE').count()
+
+        # กำหนดสถานะ Engagement ตามสถานะของ Task
+        if done > 0:
+            self.status = 'DONE'
+        elif pending_client > 0:
+            self.status = 'PENDING CLIENT'
+        elif review > 0:
+            self.status = 'REVIEW'
+        elif in_progress > 0:
+            self.status = 'IN PROGRESS'
+        else:
+            self.status = 'OPEN JOB'
+
+        # บันทึกการเปลี่ยนแปลง
+        self.save()
+
+class EngagementCategory(models.Model):
+    name_th = models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_en = models.CharField(max_length=255, default=None, null=True, blank=True)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'engagement_category'
+
+class EngagementDetail(models.Model):
+    engagement = models.ForeignKey(Engagement, on_delete=models.CASCADE)
+    engagement_category = models.ForeignKey('EngagementCategory', on_delete=models.CASCADE, related_name='engagement_detail_category', null=True, blank=True)
+    engagement_type = models.ForeignKey('EngagementType', on_delete=models.CASCADE, related_name='engagement_detail_type', null=True, blank=True)
+    type = models.CharField(max_length=255, default=None, null=True, blank=True)
+    deadline = models.DateField(null=True, blank=True)
+    notification = models.CharField(max_length=255, default=None, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    review_by = models.BooleanField(default=False)
+    approved_by = models.BooleanField(default=False)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'engagement_detail'
+        
+class EngagementType(models.Model):
+    name_th = models.CharField(max_length=255, default=None, null=True, blank=True)
+    name_en = models.CharField(max_length=255, default=None, null=True, blank=True)
+    description = models.CharField(max_length=255, default=None, null=True, blank=True)
+    category = models.ForeignKey(EngagementCategory, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'engagement_type'
+
+class Task(models.Model):
+    OPENJOB = 'OPEN JOB'
+    IN_PROGRESS = 'IN PROGRESS'
+    REVIEW = 'REVIEW'
+    PENDINGCLIENT = 'PENDING CLIENT'
+    DONE = 'DONE'
+
+    STATUS_CHOICES = [
+        (OPENJOB, 'Open Job'),
+        (IN_PROGRESS, 'In Progress'),
+        (REVIEW, 'Review'),
+        (PENDINGCLIENT, 'Pending Client'),
+        (DONE, 'Done'),
+    ]
+    engagement = models.ForeignKey(Engagement, on_delete=models.CASCADE)
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default=OPENJOB)
+    created_at = models.DateTimeField(default=timezone.now)
+    created_by = models.IntegerField()
+    updated_at = models.DateTimeField(null=True, blank=True)
+    updated_by = models.IntegerField(null=True, blank=True)
+    deleted_at = models.DateTimeField(null=True, blank=True)
+    deleted_by = models.IntegerField(null=True, blank=True)
+    voided_at = models.DateTimeField(null=True, blank=True)
+    voided_by = models.IntegerField(null=True, blank=True)
+
+    class Meta:
+        db_table = 'task'
+
+    def update_engagement_status(self):
+        if self.status != Task.OPENJOB:
+            engagement = self.engagement
+            engagement.status = self.status
+            engagement.save()
--- pconnectacc/taskcontrol/urls.py
+++ pconnectacc/taskcontrol/urls.py
@@ -1,8 +1,76 @@
 from django.urls import path
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
-App_name = "taskcontrol"
+app_name = "taskcontrol"
 
 urlpatterns = [
     path('', home),
+
+    # path('error404/', error_page404, name="error_page404"),
+    # path('login/', login_view, name="login"),
+    # path('logout/', logout_view, name="logout"),
+    # path('user_view/', user_view, name="user_view"),
+    # path('sign_up/', sign_up, name="sign_up"),
+    # path('error404/', error_page, name="error_page"),
+
+    # path('testpage/', testpage, name='testpage'),
+    # path('test2/', test, name='test'),
+
+    # path('get_districts/', get_districts, name='GetDistrict'),
+    # path('get_subdistricts/', get_subdistricts, name='GetSubdistrict'),
+    # path('get_latest_job_code/', get_latest_job_code, name='get_latest_job_code'),
+
+    # path('get_engagement_type/', get_engagement_type, name='GetEngagementType'),
+
+    # path('dashboard/', dashboard, name='dashboard'),
+    # path('get_engagement_data/', get_engagement_data, name='get_engagement_data'),
+    # path('search_datatable/', search_datatable, name='search_datatable'),
+    # path('get_engagement_summary/', get_engagement_summary, name='get_engagement_summary'),
+    # path('get_engagement_accounting/', get_engagement_accounting, name='get_engagement_accounting'),
+
+    # path('client/list/', client_list, name='client_list'),
+    # path('client/create/', client_create, name='client_create'),
+    # path('client/detail/<int:client_id>/', client_detail, name='client_detail'),
+    # path('client/update/<int:client_id>/', client_update, name='client_update'),
+    # path('delete/client/<int:client_id>/', delete_client, name='delete_client'),
+
+    # path('file/list/<int:client_id>/', file_list, name='file_list'),
+    # path('create/file/<int:client_id>', create_file, name='create_file'),
+    # path('delete/file/<int:file_id>/', delete_file, name='delete_file'),
+    # path('download_file/<int:file_id>/', download_file, name='download_file'),
+    # path('download_image/<int:image_id>/', download_image, name='download_image'),
+
+    # path('client/contact/create/<int:client_id>/', create_contact, name='create_contact'),
+    # path('client/contact/delete/<int:contact_id>/', delete_contact, name='delete_contact'),
+
+    # path('passwords/<int:client_id>/', password_list, name='password_list'),
+    # path('password/create/<int:client_id>/', create_password, name='create_password'),
+    # path('password/update/<int:password_id>/', update_password, name='update_password'),
+    # path('password/delete/<int:password_id>/', delete_password, name='delete_password'),
+
+    # path('engagement-list/', engagement_list, name='engagement_list'),
+    # path('engagements/create/', engagement_create, name='engagement_create'),
+    # path('engagements/<int:engagement_id>/', engagements, name='engagements'),
+    # path('engagements/update/<int:engagement_id>/', engagement_update, name='engagement_update'),
+    # path('engagements/delete/<int:engagement_id>/', engagement_delete, name='engagement_delete'),
+
+    # path('engagement/detail/create/<int:engagement_id>/', engagement_detail_create, name='engagement_detail_create'),
+    # path('engagement/detail/delete/<int:engagement_detail_id>/', engagement_detail_delete, name='engagement_detail_delete'),
+
+    # path('setting/', setting, name='setting'),
+    # path('user_management/', user_management, name='user_management'),
+    # path('setting/category/', category, name='category'),
+    # path('setting/category/create_category', create_category, name='create_category'),
+    # path('setting/category/create_type', create_type, name='create_type'),
+
+    # path('kanban_board/', kanban_board, name='kanban_board'),
+    # path('update_engagement_status/', update_engagement_status, name='update_engagement_status'),
+    # path('update_notes/', update_engagement_notes, name='update_engagement_notes'),
+    # path('get_engagement_details/', get_engagement_details, name='get_engagement_details'),
+    # path('filter_engagement_details/', filter_engagement_details, name='filter_engagement_details'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
