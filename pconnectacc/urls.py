c8d7aff6f98a00196073aa3d3e1db94df97533ae
Montira Sukpirom <104551815+MSukpirom@users.noreply.github.com>
2024-05-30 23:35:01 +0700

    Delete pconnectacc directory

--- pconnectacc/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-from taskcontrol import views
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('task-control/', include('taskcontrol.urls')),
-]
